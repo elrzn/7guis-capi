3e96fc77620196ae88a3cbde4a5a1f5c6b9c9f42
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-10-17 18:56:32 +0200

    Use update method for temperature panes

--- temperature-converter.lisp
+++ temperature-converter.lisp
@@ -20,23 +20,32 @@ C = (F - 32) * (5/9)"
 
 (defclass temperature-pane-fahrenheit (temperature-pane) ())
 
-(defun temperature-pane-update (new-text pane interface text-length)
-  (when (> text-length 0)
-    (let ((temperature (nth-value 0 (parse-integer new-text))))
-      (when temperature
-        (temperature-pane-update-other pane interface temperature)))))
+(defmethod temperature-pane-temperature ((pane temperature-pane))
+  (nth-value 0 (parse-integer (capi:text-input-pane-text pane) :junk-allowed t)))
 
-(defgeneric temperature-pane-update-other (temperature pane interface))
+(defmethod temperature-pane-temperature-update ((pane temperature-pane) new-temperature)
+  (let ((old-temperature (temperature-pane-temperature pane)))
+    (when (or (not old-temperature)
+              (not (= old-temperature new-temperature)))
+      (setf (capi:text-input-pane-text pane) (write-to-string new-temperature)))))
+
+(defun temperature-pane-callback (new-text pane interface text-length)
+  (declare (ignore text-length))
+  (let ((temperature (nth-value 0 (parse-integer new-text :junk-allowed t))))
+    (when temperature
+      (temperature-pane-update-other pane interface temperature))))
+
+(defgeneric temperature-pane-update-other (pane interface temperature))
 
 (defmethod temperature-pane-update-other ((pane temperature-pane-celsius) interface temperature)
-  (let ((other-pane (temperature-converter-pane-fahrenheit interface)))
-    (setf (capi:text-input-pane-text other-pane)
-          (write-to-string (celsius->fahrenheit temperature)))))
+  (let ((other-pane (temperature-converter-pane-fahrenheit interface))
+        (new-temperature (celsius->fahrenheit temperature)))
+    (temperature-pane-temperature-update other-pane new-temperature)))
 
 (defmethod temperature-pane-update-other ((pane temperature-pane-fahrenheit) interface temperature)
-  (let ((other-pane (temperature-converter-pane-celsius interface)))
-    (setf (capi:text-input-pane-text other-pane)
-          (write-to-string (fahrenheit->celsius temperature)))))
+  (let ((other-pane (temperature-converter-pane-celsius interface))
+        (new-temperature (fahrenheit->celsius temperature)))
+    (temperature-pane-temperature-update other-pane new-temperature)))
 
 (capi:define-interface temperature-converter ()
   ()
@@ -44,11 +53,11 @@ C = (F - 32) * (5/9)"
    (celsius temperature-pane-celsius
             :title "Celsius"
             :accessor temperature-converter-pane-celsius
-            :change-callback #'temperature-pane-update)
+            :change-callback #'temperature-pane-callback)
    (fahrenheit temperature-pane-fahrenheit
                :title "Fahrenheit"
                :accessor temperature-converter-pane-fahrenheit
-               :change-callback #'temperature-pane-update))
+               :change-callback #'temperature-pane-callback))
   (:layouts
    (main capi:row-layout '(celsius fahrenheit)))
   (:default-initargs :title "TempConv"))
