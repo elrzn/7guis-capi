550b10edcb18b387d5878fa16e4af6441c6e98d6
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-10-18 21:16:44 +0200

    Fix precision on year and implement change callback

--- 3-flight-booker.lisp
+++ 3-flight-booker.lisp
@@ -11,7 +11,7 @@
       (when (= 3 (length tokens))
         (destructuring-bind (day month year)
             (mapcar #'parse-integer tokens)
-          (encode-universal-time 0 0 0 day month year))))))
+          (encode-universal-time 0 0 0 day month (+ 2000 year)))))))
 
 (declaim (ftype (function (fixnum) string) date->string))
 
@@ -21,14 +21,23 @@
       (second minute hour day month year day-of-week dst-p tz)
       (decode-universal-time given-date)
     (declare (ignore second minute hour day-of-week dst-p tz))
-    (format nil "~d.~d.~d" day month year)))
+    (format nil "~d.~d.~d" day month (abs (- 2000 year)))))
+
+(defun change-callback (new-text pane interface text-length)
+  (declare (ignore interface text-length))
+  (setf (capi:simple-pane-background pane) (if (string->date new-text) nil :red)))
 
 (capi:define-interface flight-booker ()
   ()
   (:panes
    (ticket-type capi:option-pane :items '("one-way flight" "return flight"))
-   (start-date capi:text-input-pane :text (date->string (get-universal-time)))
-   (arrival-date capi:text-input-pane :text "" :enabled nil)
+   (start-date capi:text-input-pane
+               :text (date->string (get-universal-time))
+               :change-callback #'change-callback)
+   (arrival-date capi:text-input-pane
+                 :text ""
+                 :change-callback #'change-callback
+                 :enabled nil)
    (book capi:push-button :text "Book"))
   (:layouts
    (main capi:column-layout '(ticket-type start-date arrival-date book)))
