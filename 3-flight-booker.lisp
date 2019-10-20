4fc5a3bf1d00b6c204b610dd09635d3f4a803c50
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-10-20 15:23:04 +0200

    Use symbols for ticket types and enable return type ticket

--- 3-flight-booker.lisp
+++ 3-flight-booker.lisp
@@ -4,6 +4,8 @@
 
 (declaim (ftype (function (string) fixnum) string->date))
 
+(defparameter +flight-ticket-options+ '(one-way-flight return-flight))
+
 (defun string->date (given-string)
   (declare (string given-string))
   (when (= 8 (length given-string))
@@ -28,21 +30,29 @@
 (defmethod get-date ((pane flight-booker-date))
   (string->date (capi:text-input-pane-text pane)))
 
-(defun change-callback (new-text pane interface text-length)
+(defun flight-booker-date-change-callback (new-text pane interface text-length)
   (declare (ignore new-text interface text-length))
   (setf (capi:simple-pane-background pane)
         (if (get-date pane) :white :red)))
 
+(defun flight-booker-ticket-selection-callback (ticket-type interface)
+  (let ((pane (flight-booker-arrival-date interface)))
+    (setf (capi:simple-pane-enabled pane) (not (eq ticket-type 'one-way-flight)))))
+
 (capi:define-interface flight-booker ()
   ()
   (:panes
-   (ticket-type capi:option-pane :items '("one-way flight" "return flight"))
+   (ticket-type capi:option-pane
+                :items +flight-ticket-options+
+                :print-function #'string-downcase
+                :selection-callback #'flight-booker-ticket-selection-callback)
    (start-date flight-booker-date
                :text (date->string (get-universal-time))
-               :change-callback #'change-callback)
+               :change-callback #'flight-booker-date-change-callback)
    (arrival-date flight-booker-date
                  :text ""
-                 :change-callback #'change-callback
+                 :change-callback #'flight-booker-date-change-callback
+                 :accessor flight-booker-arrival-date
                  :enabled nil)
    (book capi:push-button :text "Book"))
   (:layouts
