a2b2aed89be4a313d37429ea0fe0127d7985bba4
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-10-22 21:53:07 +0200

    Create #'tick and update displayed seconds

--- 4-timer.lisp
+++ 4-timer.lisp
@@ -3,7 +3,7 @@
 (in-package #:7guis-capi)
 
 (capi:define-interface progress ()
-  ()
+  ((elapsed-seconds :initform 0 :type string :accessor progress-elapsed-seconds))
   (:panes
    (progress-bar-label
     capi:display-pane
@@ -13,7 +13,7 @@
     capi:progress-bar
     :start 0
     :end 30)
-   (elapsed-seconds
+   (elapsed-seconds-display
     capi:display-pane
     :background :transparent
     :text "0s")
@@ -25,8 +25,17 @@
     capi:push-button
     :text "Reset"))
   (:layouts
-   (main capi:column-layout '(progress-bar-layout elapsed-seconds slider reset))
+   (main capi:column-layout '(progress-bar-layout elapsed-seconds-display slider reset))
    (progress-bar-layout capi:row-layout '(progress-bar-label progress-bar)))
   (:default-initargs
    :title "Timer"
    :best-width 480))
+
+(defun tick (interface)
+  (capi:execute-with-interface
+   interface
+   #'(lambda ()
+       (with-slots (elapsed-seconds progress-bar elapsed-seconds-display slider) interface
+         ;; Update elapsed seconds message.
+         (setf (capi:display-pane-text elapsed-seconds-display)
+               (format nil "~ds" (incf elapsed-seconds)))))))
